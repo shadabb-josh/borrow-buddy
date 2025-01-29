4f46093288a8ac37800407f60c7e9ab0ebfdc34e
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-01-29 11:15:13 +0530

    fixes

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,6 @@
 class Loan < ApplicationRecord
   # Validations
-  validates :borrower_id, :lender_id, :amount, :interest, :purpose, :repayment_till, :expected_return, :status, presence:true
+  validates :borrower_id, :lender_id, :amount, :interest, :purpose, :repayment_till, :expected_return, :status, presence: true
 
   # Enum for loan status
   enum status: { pending: 0, approved: 1, funded: 3, repaid: 2 }
