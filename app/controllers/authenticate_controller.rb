9e5648d9baa3a7cb800a929c533d89eeedb1e428
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-03-02 17:18:33 +0530

    fixes

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -22,7 +22,7 @@ class AuthenticateController < ApplicationController
     @user = User.find_by(email: params[:email])
 
     if @user.nil?
-      return render json: { error: "user.email_not_found" }, status: :not_found
+      return render json: { error: I18n.t("user.email_not_found") }, status: :not_found
     end
 
     if @user&.authenticate(params[:password])
